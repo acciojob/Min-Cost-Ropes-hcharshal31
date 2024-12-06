c3903fceee560b03248e7d5e616450e783855710
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2024-12-06 15:52:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function mincost(arr)
 { 
   arr.sort((a, b) => a - b);
-	let totalCost = 0
-	while(arr.length > 1){
-		firstSmall = arr.shift();
-		secondSmall = arr.shift();
-		let cost = firstSmall + secondSmall;
-		totalCost += cost;
-		arr.push(cost);
-		arr.sort();
-	}
-	return totalCost;
+  let totalCost = 0
+  while(arr.length > 1){
+    let firstSmall = arr.shift();
+    let secondSmall = arr.shift();
+    let cost = firstSmall + secondSmall;
+    totalCost += cost;
+    arr.push(cost);
+    arr.sort((a, b) => a - b); // Add comparator here
+  }
+  return totalCost;
 }
 
 module.exports=mincost;
